02399a66d91f7566780eb7ec657a68b3fb59404c
TJSteel <jaysteel78@msn.com>
2022-10-20 00:00:00 +0100

    Initial Commit

---
+++ blackjack/blackjack.js
@@ -0,0 +1,63 @@
+const suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
+class Card {
+  value;
+  suit;
+  constructor(value, suit) {
+    this.value = value;
+    this.suit = suit;
+  }
+  getValueName() {
+    switch (this.value) {
+      case 1:
+        return 'Ace';
+      case 11:
+        return 'Jack';
+      case 12:
+        return 'Queen';
+      case 13:
+        return 'King';
+      default:
+        return '' + this.value;
+    }
+  }
+  print() {
+    console.log(`${this.getValueName()} of ${this.suit}`);
+  }
+}
+
+class Deck {
+  cards = [];
+  constructor() {
+    this.createDeck();
+  }
+  createDeck() {
+    this.cards = [];
+    for (let value = 1; value < 14; value++) {
+      for (const suit of suits) {
+        const card = new Card(value, suit);
+        this.cards.push(card);
+      }
+    }
+  }
+  getCard() {
+    if (this.cards.length == 0) {
+      throw new Error('No cards left in the deck');
+    }
+    let rnd = Math.floor(Math.random() * (this.cards.length - 1));
+    return this.cards.splice(rnd, 1)[0];
+  }
+  getCards(count) {
+    const cards = [];
+    while (count > 0) {
+      cards.push(this.getCard());
+      count--;
+    }
+    return cards;
+  }
+}
+
+const deck = new Deck();
+const playerCards = deck.getCards(2);
+for (const card of playerCards) {
+  card.print();
+}
